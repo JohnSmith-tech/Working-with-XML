1222e19edd72fcf31884b89c1931d0779e9d8b00
johnsmith <iea2001@mail.ru>
2023-01-12 18:18:08 +0700

    Start app

--- src/main/java/com/library/Main.java
+++ src/main/java/com/library/Main.java
@@ -1,5 +1,39 @@
-package com.library;public class Main {
+package com.library;
+
+import com.library.model.Book;
+import com.library.dom.XmlDomLibrary;
+import com.library.sax.XmlSaxLibrary;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        usingDOM();
+        usingSAX();
+    }
+
+    public static void usingDOM() {
+        XmlDomLibrary file = new XmlDomLibrary("books.xml");
+        List<Book> books = file.getDataFromXmlFile();
+        System.out.println(books);
+        books.add(new Book("John", "The run", "Math", "49.25", "19.12.1997", "" +
+                "Cars, guns and mafia"));
+        file.addElementToXmlFile(books);
+        System.out.println(file.getDataByKeyword("Corets, Eva"));
+    }
+
+    public static void usingSAX() {
+        XmlSaxLibrary file;
+        try {
+            file = new XmlSaxLibrary("books.xml");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        file.getDataFromXmlFile();
+        file.getDataByKeyword("Corets, Eva");
+
     }
 }
